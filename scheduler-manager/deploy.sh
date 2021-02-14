4ce60b6dc17a47dba4ab9f53d230e8ecf9b04165
Julio Renner <julio.renner@sap.com>
2021-02-14 09:00:00 -0300

    fix deploy

--- scheduler-manager/deploy.sh
+++ scheduler-manager/deploy.sh
@@ -2,6 +2,10 @@
 
 DOCKER_REPOSITORY=$1
 
+if [ -z "$var" ];
+    DOCKER_REPOSITORY="10.43.0.201:5000"
+fi
+
 docker build . -t ${DOCKER_REPOSITORY}/algorithm-scheduler:1
 
 kubectl apply -f deployment.yaml
