456a18fa5c2f55274179e3a75f25d7eb26500c1b
Julio Renner <julio.renner@sap.com>
2021-02-14 09:50:01 -0300

    update ng-ran-model image

--- placement-algorithms/ng-ran-model/build.sh
+++ placement-algorithms/ng-ran-model/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker build -t ng-ran-model:latest .
+DOCKER_REPOSITORY=$1
+
+if [ -z "$DOCKER_REPOSITORY" ]; then
+    DOCKER_REPOSITORY="10.43.0.201:5000";
+fi
+
+docker build -t ${DOCKER_REPOSITORY}/ng-ran-model:latest .
