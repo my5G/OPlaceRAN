13a4d9bd2196d4aebf76f3e0a96f39a2d34f594b
Julio Renner <julio.renner@sap.com>
2021-02-14 09:47:17 -0300

    fix repositories

--- scheduler-manager/deploy.sh
+++ scheduler-manager/deploy.sh
@@ -2,8 +2,8 @@
 
 DOCKER_REPOSITORY=$1
 
-if [ -z "$var" ];
-    DOCKER_REPOSITORY="10.43.0.201:5000"
+if [ -z "$DOCKER_REPOSITORY" ]; then
+    DOCKER_REPOSITORY="10.43.0.201:5000";
 fi
 
 docker build . -t ${DOCKER_REPOSITORY}/algorithm-scheduler:1
