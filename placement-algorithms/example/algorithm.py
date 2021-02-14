de687a95875e8ad5a85b8bf21ca69016b211466b
Julio Renner <julio.renner@sap.com>
2021-02-14 12:01:28 -0300

    fix

--- placement-algorithms/example/algorithm.py
+++ placement-algorithms/example/algorithm.py
@@ -5,6 +5,6 @@ from utils import output_result, initial_validation
 initial_validation()
 
 # Placement algorithm execution
-time.sleep(60*15)
+# time.sleep(60*15)
 
-output_result([{"id": "test_data"}])
+output_result([{"id": "1", "ru": "16", "cu": "16", "du": "16"}])
