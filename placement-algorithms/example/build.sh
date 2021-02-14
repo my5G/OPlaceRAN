6278eec71f44024335418e52edd1159fc89dd639
Julio Renner <julio.renner@sap.com>
2021-02-14 11:34:30 -0300

    fix: build.sh

--- placement-algorithms/example/build.sh
+++ placement-algorithms/example/build.sh
@@ -6,6 +6,6 @@ if [ -z "$DOCKER_REPOSITORY" ]; then
     DOCKER_REPOSITORY="10.43.0.201:5000";
 fi
 
-docker build -t ${DOCKER_REPOSITORY}/example:latest
+docker build -t ${DOCKER_REPOSITORY}/example:latest .
 
 docker push ${DOCKER_REPOSITORY}/example:latest
