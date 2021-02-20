ad75f149b9cd6f220e30569a8a45b4d4d9259fa3
Gustavo Zanatta Bruno <31906799+zanattabruno@users.noreply.github.com>
2021-02-20 17:25:31 -0400

    Update Graphics

--- performance-analysis/graphics/cluster_resources_time/plot_cluster.py
+++ performance-analysis/graphics/cluster_resources_time/plot_cluster.py
@@ -3,23 +3,13 @@ import matplotlib as mlp
 import matplotlib.pyplot as plt
 import os
 import numpy as np
-
 os.chdir('performance-analysis/graphics/cluster_resources_time/')
-
 dataframe = pd.read_csv('Cluster_Resources.csv',  delimiter=';', header=0)
-plt.plot(dataframe['Time'],dataframe['Media_CPU'], label='% Media CPU')
-plt.plot(dataframe['Time'],dataframe['Media_MEM'], label='% Media MEM')
+plt.plot(dataframe['Time'],dataframe['Media_CPU'], label='% Media CPU',color='b')
+plt.plot(dataframe['Time'],dataframe['Media_MEM'], label='% Media MEM',color='r')
 plt.legend()
 plt.xlabel("Tempo")
 plt.ylabel("% Consumode Recursos")
 plt.title("Consumo de Recursos Médio por Nó K8S em função do Tempo")
-plt.show()
-
-
-
-
-
-
-print(dataframe)
-
+plt.savefig('out/Cluster_Resources.png')
 plt.show()
