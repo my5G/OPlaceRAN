b416acdc76e2c448c628d817dede503c64d6da68
Gustavo Zanatta Bruno <31906799+zanattabruno@users.noreply.github.com>
2021-02-24 17:15:33 -0400

    insert param to change fontsize

--- performance-analysis/graphics/cluster_resources_time/plot_cluster_stderror_CPU.py
+++ performance-analysis/graphics/cluster_resources_time/plot_cluster_stderror_CPU.py
@@ -7,24 +7,24 @@ dataframe = pd.read_csv('Cluster_Resources_Media.csv',  delimiter=';', header=0,
 print(dataframe)
 ax = plt.gca()
 
+
+#Print Lines
 dataframe.plot(kind='line', y='CPU Media',color='darkblue', ax=ax)
 dataframe.plot(kind='line', y='CPU Desvio Sup',color='darkblue', linestyle='dotted', alpha=0.5,label='Desvio CPU', ax=ax)
 dataframe.plot(kind='line', y='CPU Desvio Inf',color='darkblue', linestyle='dotted', alpha=0.5, label='_nolegend_', ax=ax)
 
 
-
-
-#ax = dataframe.plot(y='CPU Media', color='darkblue')
-#ax.set_ylabel("Consumo dos RCs (%)")
-#dataframe.plot(y='RAM Media', ax=ax, color='darkred')
+#Print Vertical Lines
 ax.yaxis.grid(color='gray', linestyle='--', linewidth=0.5)
 ax.set_xlabel("Tempo (s)")
 plt.vlines(120, ymin=0, ymax=max(dataframe['CPU Desvio Sup'].to_list()), linestyles='dashed',label='Início Alocação', colors='darkblue', alpha=0.5)
 plt.vlines(480, ymin=0, ymax=max(dataframe['CPU Desvio Sup'].to_list()), linestyles='dashed',label='Fim Alocação', colors='darkred', alpha=0.5)
 plt.legend()
 
-plt.xlabel("Tempo (s)")
-plt.ylabel("Consumode Recursos (%)")
+
+#Print Labels
+plt.xlabel("Tempo (s)", fontsize=20)
+plt.ylabel("Consumode Recursos (%)",fontsize=20)
 #plt.title("Consumo de Recursos Médio por Nó K8S em função do Tempo")
 plt.savefig('out/Cluster_Resources_StdError_CPUv1.pdf')
 
