4bfd09e855224ef5eeb4258792bd3dc1e473cf9c
Julio Renner <juliorenner123@gmail.com>
2021-03-01 23:00:27 -0400

    Added test script

---
+++ performance-analysis/tests/test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#vars
+PLACER="/home/vmadmin/RANPlacer/k8s-operators/config/samples/ran_v1alpha1_ranplacer.yaml"
+TESTE_NUMBER="4"
+COLLECT_INTERVAL=5
+EACH_TEST_DURATION=900
+NUMBER_OF_TESTS=10
+mkdir "$TESTE_NUMBER"
+i=1
+s=0
+while [ $i -le $NUMBER_OF_TESTS ]
+do
+    DIR="$TESTE_NUMBER/$(date +%Y-%m-%d_%H:%M)/"
+    mkdir "$DIR"
+    #echo "$PLACER"
+    kubectl apply -f "$PLACER"
+    while [ $s -le $EACH_TEST_DURATION ]
+    do
+        kubectl top nodes > "$DIR"it_"$i"_nodes_"$s"s.txt && kubectl top pods > "$DIR"it_"$i"_pods_"$s"s.txt && sleep "$COLLECT_INTERVAL"
+        s=$(( $s + $COLLECT_INTERVAL ))
+        #echo $s
+    done
+    kubectl get randeployer --all-namespaces > "$DIR"pods_location.txt
+    kubectl delete ranplacer ranplacer-sample && sleep 180
+    i=$(( $i + 1 ))
+    #echo $i
+    s=0
+done
