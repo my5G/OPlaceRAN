54ebd1c3fd4e28017c2898d1fb10044d5d3f5ffb
Gustavo Zanatta Bruno <gkbsa2504@gmail.com>
2022-01-17 23:11:51 -0400

    add get iperf

--- images/ran/k8s-nfapi/ru/get_ping.sh
+++ images/ran/k8s-nfapi/ru/get_ping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "get_ping | Sleeping to wait interface oaitun_ue1 up."
 
-sleep 120
+sleep 60
 
 echo "get_ping | Creating output file $HOSTNAME.txt"
 
@@ -10,10 +10,13 @@ touch $HOSTNAME.txt
 
 echo "get_ping | Ping from interface oaitun_ue1."
 
-iperf -c 10.41.101.100 -i 1 -B 45.45.0.2 -t 90 >> $HOSTNAME.txt
+unlink /etc/localtime   
+cp /usr/share/zoneinfo/America/Cuiaba /etc/localtime
+date >> $HOSTNAME.txt
+iperf -c 10.41.101.100 -i 1 -B 45.45.0.2 -t 60 >> $HOSTNAME.txt
 
 echo "get_ping | Send file to collector."
 
 chmod 400 /root/.ssh/id_rsa
 
-scp -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa $HOSTNAME.txt vmadmin@10.43.0.201:tests/results
+scp -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa $HOSTNAME.txt vmadmin@10.41.101.100:tests/results
